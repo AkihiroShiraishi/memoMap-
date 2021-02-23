79e74e803124161e6b1a6edcce1792e4eeaecd85
AkihiroShiraishi <ws.6918rockmusic@icloud.com>
2021-02-23 09:57:42 +0900

    first commit

---
+++ memoMapβ/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  memoMapβ
+//
+//  Created by 白石晃大 on 2021/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
